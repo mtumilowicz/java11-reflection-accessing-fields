3fe7737ef785f7ddf2f56a88aa5a860493a29e74
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-05 20:36:10 +0100

    class + test scaffold

---
+++ src/main/java/X.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mtumilowicz on 2019-02-05.
+ */
+class X {
+    private String privateField = "a";
+    int count = 0;
+    public String name = "public";
+}
---
+++ src/test/java/AccessingFieldsTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by mtumilowicz on 2019-02-05.
+ */
+public class AccessingFieldsTest {
+
+}
